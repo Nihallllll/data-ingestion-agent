a56a6c483c5b24d02e3361336b77f9531e3b16b5
Nihallllll <nihal24rajak@gmail.com>
2026-01-25 20:22:09 +0530

    test

--- github-pipeline/index.ts
+++ github-pipeline/index.ts
@@ -1,14 +1,36 @@
+import { file } from "bun"
 import express from "express"
 
 const app = express()
 app.use(express.json())
 
-app.post("/github" ,(req,res)=>{
+app.post("/github", async (req,res)=>{
     console.log("Github send a message")
     const data  = req.body;
-    console.log("data ",data)
-    console.log("Repo name :" , data.repository.name)
-    console.log("Pushed by :",data.pusher.name )
+    
+    const filesModified = data.head_commit.modified;
+
+    if (filesModified.length > 0){
+        for(const file in filesModified){
+            console.log("reading files")
+
+            const owner = data.repository.owner.login;
+            const repo = data.repository.name;
+            const branch = data.ref.replace("refs/heads/", "")
+
+            const rawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${file}`;
+            try  {
+                const response = await fetch(rawUrl)
+                const text = await response.text()
+
+                console.log("file data :" , text)
+
+            }catch{
+                console.log("get some help")
+            }    
+        }
+    }
+    res.send("OK")
 })
 
 app.listen(3000, ()=> console.log("Server is runnign on port 3000"));
